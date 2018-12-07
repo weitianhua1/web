baf75a33055c2a172517f6eec06144f1ba18cbda
weitianhua1 <468591275@qq.com>
2018-12-07 12:10:06 +0800

    as

--- WebApplication1/WebApplication1/Controllers/TestController.cs
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using WebApplication1.Models;
+using WebApplication1.Models; 
+
 
 namespace WebApplication1.Controllers
 {
@@ -12,14 +13,48 @@ namespace WebApplication1.Controllers
         // GET: Test
         public string GetString()
         {
-            return  "hello";
+            return "hello";
         }
         public Customer GetCustomer()
         {
             Customer ct = new Customer();
-            ct.CustomerName = "abc";
-            ct.Address = "mvc";
+            ct.CustomerName = "QAQ";
+            ct.Address = "QAQ";
             return ct;
+        }
+        public ActionResult GetView()
+        {
+            //    // 获取当前时间
+            //    if (DateTime.Now.Hour > 12)
+            //    {
+            //        return View("YourView");
+            //    }
+            //    else
+            //    {
+            //        return View("MyView");
+            //    }
+            string greeting;
+            DateTime dt = DateTime.Now;
+            int hour = dt.Hour;
+            if (hour < 12)
+            {
+                greeting = "早上好";
+            }
+            else
+            {
+                greeting = "下午好";
+            }
+            ViewData["greeting"] = greeting;
+        Employee emp = new Employee();
+
+            emp.Name = "三八";
+    
+            emp.Salary = 46859;
+            ViewData["EmpKey"] = emp;
+
+
+            return View("MyView");
+
         }
     }
 }
