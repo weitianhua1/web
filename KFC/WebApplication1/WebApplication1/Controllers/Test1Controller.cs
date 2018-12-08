d9bab8f8b881acd4c6b0736f0e377b266eb2b648
weitianhua1 <468591275@qq.com>
2018-12-08 12:06:31 +0800

    。

---
+++ KFC/WebApplication1/WebApplication1/Controllers/Test1Controller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class Test1Controller : Controller
+    {
+        // GET: Test1
+        public ActionResult GetView()
+        {
+            string greeting;
+            DateTime dt = DateTime.Now;
+            int hour = dt.Hour;
+
+            if (hour < 12)
+            {
+                greeting = "早上好";
+            }
+            else
+            {
+                greeting = "下午好";
+            }
+
+            //ViewData["greeting"] = greeting;
+            ViewBag.greeting = greeting;
+            Customer emp = new Customer();
+            emp.CustomerNmae = "顾客A";
+            emp.Address = "南宁";
+            //ViewData["Empkey"] = emp;
+            ViewBag.Empkey = emp;
+            return View("GetView", emp);
+        }
+    }
+}
---
+++ KFC/WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        public string GetString()
+        {
+            return "hello wrold";
+        }
+        public Customer getCustomer()
+        {
+            Customer ct = new Customer();
+            ct.CustomerNmae = "abc";
+            ct.Address = "def";
+            return ct;
+        }
+        public ActionResult GetView()
+        {
+            string greeting;
+            DateTime dt = DateTime.Now;
+            int hour = dt.Hour;
+
+            if (hour < 12)
+            {
+                greeting="早上好";  
+            }
+            else
+            {
+                greeting="下午好";
+            }
+
+            //ViewData["greeting"] = greeting;
+            ViewBag.greeting = greeting;
+            Employee emp = new Employee();
+            emp.Name = "李四";
+            emp.Salary = 20002;
+            //ViewData["Empkey"] = emp;
+            ViewBag.Empkey = emp;
+            return View("MyView",emp);
+        }
+    }
+}
