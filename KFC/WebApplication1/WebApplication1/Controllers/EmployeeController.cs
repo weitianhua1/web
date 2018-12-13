6751db0fce68a1eeea78752cd55fd466950fab19
weitianhua1 <468591275@qq.com>
2018-12-13 18:16:34 +0800

    2

--- KFC/WebApplication1/WebApplication1/Controllers/EmployeeController.cs
+++ KFC/WebApplication1/WebApplication1/Controllers/EmployeeController.cs
@@ -13,55 +13,68 @@ namespace WebApplication1.Controllers
         // GET: Employee
         public ActionResult Index()
         {
+            EmployeeListViewModel empListModel = new EmployeeListViewModel();
+
+
+
+            //将处理过的数据列表送给强视图类型对象
+            empListModel.EmployeeViewList = getEmpVmList();
+            empListModel.Greeting = getGreeting();
+            empListModel.UserName = getUserName();
+
+            return View(empListModel);
+        }
+
+        [NonAction]
+        List<EmployeeViewModel> getEmpVmList()
+        {
+            //实例化员工信息业务层
+            EmployeeBusinessLayer empBL = new EmployeeBusinessLayer();
+            //员工原始数据列表，获取来自业务层类的数据
+            var listEmp = empBL.GetEmployeeList();
+            //员工原始数据加工后的视图数据列表，当前状态是空的
+            var listEmpVm = new List<EmployeeViewModel>();
+
+            foreach (var item in listEmp)
             {
-                EmployeeListViewModel empListModel = new EmployeeListViewModel();
-
-                //实例化员工信息业务层
-                EmployeeBusinessLayer empBL = new EmployeeBusinessLayer();
-                //员工原始数据列表，获取来自业务层类的数据
-                var listEmp = empBL.GetEmployeeList();
-                //员工原始数据加工后的视图数据列表，当前状态是空的
-                var listEmpVm = new List<EmployeeViewModel>();
-
-                //通过循环遍历员工原始数据数组， 讲数据一个一个的转换，并加入listEmpVm
-                foreach (var item in listEmp)
+                EmployeeViewModel empVmObj = new EmployeeViewModel();
+                empVmObj.EmployeeName = item.Name;
+                empVmObj.EmployeeSalary = item.Salary.ToString("C");
+                if (item.Salary > 2000)
                 {
-                    EmployeeViewModel empVmObj = new EmployeeViewModel();
-                    empVmObj.EmployeeName = item.Name;
-                    empVmObj.EmployeeSalary = item.Salary.ToString("C");
-                    if (item.Salary > 1000)
-                    {
-                        empVmObj.EmployeeGrade = "土豪";
-                    }
-                    else
-                    {
-                        empVmObj.EmployeeGrade = "穷人";
-                    }
-                    listEmpVm.Add(empVmObj);
-                }
-
-                //将处理过的数据列表送给强视图类型对象
-                empListModel.EmployeeViewList = listEmpVm;
-
-                string greeting;
-                //获取当前时间
-                DateTime dt = DateTime.Now;
-                //获取当前小时
-                int hour = dt.Hour;
-                if (hour < 12)
-                {
-                    greeting = "早上好";
+                    empVmObj.EmployeeGrade = "骚猪";
                 }
                 else
                 {
-                    greeting = "中午好";
+                    empVmObj.EmployeeGrade = "乞丐";
                 }
-                empListModel.Greeting = greeting;
-                empListModel.UserName = "管理员";
-                return View(empListModel);
-            
+                listEmpVm.Add(empVmObj);
             }
-           
+            return listEmpVm;
+        }
+
+        [NonAction]
+        string getGreeting()
+        {
+            string greeting;
+            //获取当前时间
+            DateTime dt = DateTime.Now;
+            //获取当前小时
+            int hour = dt.Hour;
+            if (hour < 12)
+            {
+                greeting = "早上好";
+            }
+            else
+            {
+                greeting = "中午好";
+            }
+            return greeting;
+        }
+        [NonAction]
+        string getUserName()
+        {
+            return "管理员";
         }
     }
 }
--- KFC/WebApplication1/WebApplication1/Models/EmployeeBusinessLayer.cs
+++ KFC/WebApplication1/WebApplication1/Models/EmployeeBusinessLayer.cs
@@ -12,17 +12,17 @@ namespace WebApplication1.Models
             var list = new List<Employee>();
             Employee emp = new Models.Employee();
             emp.Name = "三八";
-            emp.Salary = 288;
+            emp.Salary = 2999;
             list.Add(emp);
 
             emp = new Models.Employee();
             emp.Name = "三九";
-            emp.Salary = 288;
+            emp.Salary = 3000;
             list.Add(emp);
 
             emp = new Models.Employee();
             emp.Name = "四十一";
-            emp.Salary = 288;
+            emp.Salary = 1999;
             list.Add(emp);
 
             return list;
